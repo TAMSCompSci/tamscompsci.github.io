cdba086d532b8a9fcb06391d7b842871bc0bea68
Garrett Gu <garrettgu777@gmail.com>
2017-08-22 13:39:16 -0500

    indents leaderboard

--- jsify.js
+++ jsify.js
@@ -144,7 +144,7 @@ var loadBoard = function() {
 	$.get('http://tamscso.ga/board/rest/fetch_users', function(data, status) {
 		var people = JSON.parse(data);
 		$('.board-container').html(
-			specialify("Name" + new Array(55-4).join(' ') + "Score\n")+
+			specialify("\n\tName" + new Array(55-4).join(' ') + "Score\n\t")+
 			people.map(function(item) {
 				var name = item.profile.name;
 				var score = item.score;
@@ -154,8 +154,8 @@ var loadBoard = function() {
 				var spacesToAdd = 55-name.length;
 				name += new Array(spacesToAdd).join(' ');
 				return name + score;
-			}).join('\n')+
-			"\n\n"+linkify('Click here', 'http://tamscso.ga/board')+" to see the full leaderboard."
+			}).join('\n\t')+
+			"\n\n\t"+linkify('Click here', 'http://tamscso.ga/board')+" to see the full leaderboard."
 		);
 	})
 }
